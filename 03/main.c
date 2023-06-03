d55d96405ca09a2ec49d78ac2fb1c59940b369f9
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2023-06-03 16:30:54 +0800

    starting to write code

---
+++ 03/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int myFavoriteNumber = 0;
+    printf("Enter your favorite number: ");
+    scanf("%d", &myFavoriteNumber);
+    printf("Your favorite number is %d\n", myFavoriteNumber);
+    return 0;
+}
---
+++ 03/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi, my name is jgyy\n");
+    return 0;
+}
